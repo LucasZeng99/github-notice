a2e847c4d6fc3d7e389ff384af6c45cbbd9f65ee
Lucas Zeng <lucaszeng99@gmail.com>
2018-11-17 22:39:40 -0700

    modify router

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
 import "../App.css";
 import Navbar from "./Navbar";
 import ActivitiesView from "./ActivitiesView";
@@ -10,8 +10,8 @@ const Router = () => {
       <div className="router">
         <Navbar />
         <Switch>
-          <Route path="/" component={ActivitiesView} exact />
-          <Route path="/settings" component={SettingsView} />
+        <Route path="/settings" component={SettingsView} />
+          <Route path="/" component={ActivitiesView}/>
         </Switch>
       </div>
     </BrowserRouter>
