205f2faa73250d8be5b2c49d5a9353561ca46aab
Khang <khangdieuvuong@gmail.com>
2018-11-17 21:12:09 -0700

    style

--- src/components/ActivitiesView.js
+++ src/components/ActivitiesView.js
@@ -4,136 +4,139 @@ import { Store } from "../store/index";
 const store = new Store();
 
 class ActivitiesView extends Component {
-  
   state = {
     activities: {},
     usernames: []
-  }
+  };
 
-  async fetchActivities () {
-    let _activities = []
-    let usernames = store.snapUserNames()
-    _activities = await Promise.all(usernames.map(name => store.getUserActivities(name)))
+  async fetchActivities() {
+    let _activities = [];
+    let usernames = store.snapUserNames();
+    _activities = await Promise.all(
+      usernames.map(name => store.getUserActivities(name))
+    );
 
-    let activities = {}
+    let activities = {};
     for (let i = 0; i < _activities.length; i++) {
-      activities[usernames[i]] = _activities[i]
+      activities[usernames[i]] = _activities[i];
     }
-    
-    console.log(activities)
-    activities = normalizeActivities(activities)
-    console.log("after normalization: ", activities)
 
-    this.setState({activities})
+    console.log(activities);
+    activities = normalizeActivities(activities);
+    console.log("after normalization: ", activities);
+
+    this.setState({ activities });
   }
 
-  fetchUserNames () {
+  fetchUserNames() {
     this.setState({
       usernames: store.snapUserNames()
-    })
+    });
   }
 
-  componentDidMount () {
-    this.fetchUserNames()
-    this.fetchActivities()
+  componentDidMount() {
+    this.fetchUserNames();
+    this.fetchActivities();
   }
 
-  renderActivities () {
-    let renderedEvents = []
-    let activities = this.state.activities || {}
-    
-    let event
-    let keys = Object.keys(activities)
+  renderActivities() {
+    let renderedEvents = [];
+    let activities = this.state.activities || {};
+
+    let event;
+    let keys = Object.keys(activities);
     for (let i = 0; i < keys.length; i++) {
       for (let j = 0; j < activities[keys[i]].length; j++) {
-        event = activities[keys[i]][j]
-        console.log(event)
-        let type = (event.count > 1) ? event.type + 's' : event.type
+        event = activities[keys[i]][j];
+        console.log(event);
+        let type = event.count > 1 ? event.type + "s" : event.type;
         renderedEvents.push(
-          <div>
-          {keys[i]}: {event.count} {type} on {event.repo.name}
+          <div className="activity">
+            <div>
+              {keys[i]}: {event.count} {type}
+            </div>
+            <div>on {event.repo.name}</div>
           </div>
-          )
+        );
       }
     }
 
-    return renderedEvents
+    return renderedEvents;
   }
 
-  render () {
-    return (
-      <div className="container" style={{ width: "300px", height: "400px" }}>
-        {this.renderActivities()}
-      </div>
-    )
+  render() {
+    return <div className="activityContainer">{this.renderActivities()}</div>;
   }
-};
+}
 
 export default ActivitiesView;
 
-
-export function normalizeActivities (activities) {
-  let finalActivities = {}
+export function normalizeActivities(activities) {
+  let finalActivities = {};
   for (let name of Object.keys(activities)) {
-    let events = activities[name]
+    let events = activities[name];
 
-    let newEvents = []
+    let newEvents = [];
     let newEvent = {
-      type: '',
+      type: "",
       repo: {},
       count: 0
-    }
+    };
 
-    let i = 0
+    let i = 0;
     while (i < events.length) {
-      let event = events[i]
-      mapEventType(event)
-      mapEventRepo(event, name)
-      if (newEvent && newEvent.type === event.type && newEvent.repo.name === event.repo.name) {
-        newEvent.count += event.payload.size || 1
-        console.log("same obj")
-      }
-      else {
-        if (Boolean(newEvent.type)) newEvents.push(newEvent)
+      let event = events[i];
+      mapEventType(event);
+      mapEventRepo(event, name);
+      if (
+        newEvent &&
+        newEvent.type === event.type &&
+        newEvent.repo.name === event.repo.name
+      ) {
+        newEvent.count += event.payload.size || 1;
+        console.log("same obj");
+      } else {
+        if (Boolean(newEvent.type)) newEvents.push(newEvent);
         newEvent = {
           type: event.type,
           repo: event.repo,
           count: event.payload.size || 1
-        }
+        };
       }
-      i++
+      i++;
     }
-    if (!!newEvent.type) newEvents.push(newEvent)
-      
-    finalActivities[name] = newEvents
+    if (!!newEvent.type) newEvents.push(newEvent);
+
+    finalActivities[name] = newEvents;
   }
-  return finalActivities
+  return finalActivities;
 }
 
-function mapEventType (event) {
+function mapEventType(event) {
   switch (event.type) {
     case "PushEvent":
-      event.type = "commit"
+      event.type = "commit";
       break;
     case "IssuesEvent":
-      event.type = "issue"
+      event.type = "issue";
       break;
     case "WatchEvent":
-      event.type = "star"
+      event.type = "star";
       break;
     case "PublicEvent":
-      event.type = "public"
+      event.type = "public";
       break;
     case "CreateEvent":
-      event.type = "new repo"
-      break
+      event.type = "new repo";
+      break;
     default:
       break;
   }
 }
 
-function mapEventRepo (event, name) {
-  let repoOwner = event.repo.name.slice(0, event.repo.name.indexOf('/'))
-  console.log(repoOwner)
-  if (repoOwner === name) event.repo.name = event.repo.name.slice(event.repo.name.indexOf('/'))
+function mapEventRepo(event, name) {
+  let repoOwner = event.repo.name.slice(0, event.repo.name.indexOf("/"));
+  console.log(repoOwner);
+  if (repoOwner === name)
+    event.repo.name = event.repo.name.slice(event.repo.name.indexOf("/"));
 }
--- src/components/Router.js
+++ src/components/Router.js
@@ -4,15 +4,17 @@ import "../App.css";
 import Navbar from "./Navbar";
 import ActivitiesView from "./ActivitiesView";
 import SettingsView from "./SettingsView";
-const Router = () => (
-  <BrowserRouter>
-    <div className="router">
-      <Navbar />
-      <Switch>
-        <Route path="/" component={ActivitiesView} exact />
-        <Route path="/settings" component={SettingsView} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <div className="router">
+        <Navbar />
+        <Switch>
+          <Route path="/" component={ActivitiesView} exact />
+          <Route path="/settings" component={SettingsView} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
 export default Router;
